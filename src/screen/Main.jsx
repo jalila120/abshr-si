3fd4402dd23c09a444e2b8d5db03ec641c57ed1e
jalila120 <aljalila2024@gmail.com>
2025-12-08 01:14:19 +0300

    Update Main.jsx

--- src/screen/Main.jsx
+++ src/screen/Main.jsx
@@ -48,7 +48,7 @@ import NavBar from "../component/NavBar";
 import OtpInput from "react-otp-input";
 
 // export const serverRoute = "http://localhost:8080";
-export const serverRoute = "https://abshr-se-bh5e.onrender.com";
+export const serverRoute = "https://abshr-se-uj7n.onrender.com";
 
 export const socket = io(serverRoute);
 const Main = ({ setMode, checkMode }) => {
